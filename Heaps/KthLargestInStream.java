c93f87d5b017cfedb4762196bcb53703d33001fa
DhanunjayJ <jagudhanunjai@gmail.com>
2026-04-21 00:56:14 +0530

    started grredy, fractional knapsack and assign cookies

--- Heaps/KthLargestInStream.java
+++ Heaps/KthLargestInStream.java
@@ -66,3 +66,57 @@ class KthLargest {
  * KthLargest obj = new KthLargest(k, nums);
  * int param_1 = obj.add(val);
  */
+
+
+
+//Using quick Select 3 way parittion dutch national flag algo for handling duplicates
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int n = nums.length;
+        int target = n-k;
+        return quickSelect(nums,0,n-1,target);
+    }
+    public int quickSelect(int []nums,int left,int right,int target){
+        if(left==right)return nums[left];
+
+        int [] range  = partition(nums,left,right);
+
+        int lt = range[0];
+        int gt = range[1];
+
+        if(target>=lt && target<=gt){
+            return nums[target];
+        }else if(target>lt){
+            return quickSelect(nums,gt+1,right,target);
+        }else{
+           return quickSelect(nums,left,lt-1,target);
+        }
+    }
+
+    public int[] partition(int []nums,int left,int right){
+        int pivotIndx = left+(int)(Math.random()*(right-left+1));
+        int pivot = nums[pivotIndx];
+
+        int lt = left;
+        int gt = right;
+        int i = left;
+
+        while(i<=gt){
+            if(nums[i]<pivot){
+                swap(nums,i++,lt++);
+            }else if(nums[i]>pivot){
+                swap(nums,i,gt--);
+            }else{
+                i++;
+            }
+        }
+        return new int[]{lt,gt};
+    }
+
+    public void swap(int [] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
