0e941d1b0337665bfe16e38219da122ce2f536ef
DhanunjayJ <jagudhanunjai@gmail.com>
2026-08-28 00:18:43 +0530

    Added Qucik Sort 3 Way partitioning exp

--- Sorting/QuickSort.java
+++ Sorting/QuickSort.java
@@ -49,40 +49,49 @@ class Solution {
         return nums;
     }
 
-    private void quickSort(int[] nums, int l, int r) {
+    public void quickSort(int[] nums, int l, int r) {
         if (l >= r) return;
 
-        // 1. Randomize pivot to protect against sorted inputs
-        int randomIdx = l + (int) (Math.random() * (r - l + 1));
-        swap(nums, randomIdx, l);
+        // Randomize the pivot to avoid O(n^2) on sorted arrays
+        int random = l + (int) (Math.random() * (r - l + 1));
+        swap(nums, random, l);
 
         int pivot = nums[l];
 
-        // 2. Three-way partitioning pointers
-        int i = l;      // Tracks boundary for < pivot
-        int j = r;      // Tracks boundary for > pivot
-        int k = l + 1;  // Scans through the array
+        // 3-Way Partitioning (Dutch National Flag Pointers):
+        // [l ... i-1]   -> elements < pivot
+        // [i ... k-1]   -> elements == pivot
+        // [k ... j]     -> unexamined elements
+        // [j+1 ... r]   -> elements > pivot
+        int i = l; 
+        int j = r; 
+        int k = l + 1; 
 
         while (k <= j) {
             if (nums[k] < pivot) {
+                // If a value is smaller, put it in the '< pivot' zone.
+                // We swap with 'i' (which holds either an equal value or k itself).
                 swap(nums, i, k);
                 i++;
                 k++;
             } else if (nums[k] > pivot) {
+                // When swapping with 'j', the incoming value from the right 
+                // is completely unknown (could be <, ==, or >). 
+                // Therefore, we DO NOT increment k yet so we can inspect it next.
                 swap(nums, k, j);
-                j--; // Don't increment k yet, we need to examine the swapped element
+                j--;
             } else {
-                k++; // Equal to pivot, just move forward
+                // If equal to pivot, just expand the middle zone.
+                k++;
             }
         }
 
-        // 3. Recursively sort elements less than and greater than pivot
-        // (Notice we skip sorting the middle section because duplicates are already done!)
+        // Recursively sort sub-arrays (skipping the middle '==' block)
         quickSort(nums, l, i - 1);
         quickSort(nums, j + 1, r);
     }
 
-    private void swap(int[] nums, int i, int j) {
+    public void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
